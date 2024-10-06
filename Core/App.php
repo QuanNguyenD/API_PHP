690b999ac1c528c008bd274d95c2d4c70f2166d6
Quan <nguyenquocanhquangl@gmail.com>
2024-10-06 22:49:41 +0700

    Cập nhật

--- Core/App.php
+++ Core/App.php
@@ -1,8 +1,14 @@
 <?php
 
+use Gettext\Loader\PoLoader;
 
 class App{
 
+    protected $router;
+    protected $controller;
+    protected $plugins;
+
+    use Gettext\Loader\PoLoader;
     private function db(){
         $config = [
             'driver' => DATA_BASE,
@@ -35,6 +41,100 @@ class App{
 
     //     $query = $qb->table('my_table')->where('name', '=', 'Sana');
     // }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    private function i18n()
+    {   
+        $Route = $this->controller->getVariable("Route");
+        $AuthUser = $this->controller->getVariable("AuthUser");
+        $IpInfo = $this->controller->getVariable("IpInfo");
+
+        if ($AuthUser) {
+            // Get saved lang code for authorized user.
+            $lang = $AuthUser->get("preferences.language");
+        } else if (isset($Route->params->lang)) {
+            // Direct link or language change
+            // Getting lang from route
+            $lang = $Route->params->lang;
+        } else if (Input::cookie("lang")) {
+            // Returninn user (non-auth),
+            // Getting lang. from the cookie
+            $lang = Input::cookie("lang");
+        } else {
+            // New user
+            // Getting lang. from ip-info
+            $lang = Config::get("default_applang");
+
+            if ($IpInfo->languages) {
+                foreach ($IpInfo->languages as $l) {
+                    foreach (Config::get("applangs") as $al) {
+                        if ($al["code"] == $l || $al["shortcode"] == $l) {
+                            // found, break loops
+                            $lang = $al["code"];
+                            break 2;
+                        }
+                    }
+                }
+            }
+        }
+
+
+        // Validate found language code
+        $active_lang = Config::get("default_applang");
+        foreach (Config::get("applangs") as $al) {
+            if ($al["code"] == $lang || $al["shortcode"] == $lang) {
+                // found, break loop
+                $active_lang = $al["code"];
+                break;
+            }
+        }
+
+        define("ACTIVE_LANG", $active_lang);
+        @setcookie("lang", ACTIVE_LANG, time()+30 * 86400, "/");
+
+        
+        
+        $Translator = new Gettext\Translator;
+        $loader = new PoLoader();
+        // Load app. locale
+        $path = APPPATH . "/locale/" . ACTIVE_LANG . "/messages.po";
+        if (file_exists($path)) {
+            $translations = $loader -> loadFile($path);
+            $Translator->loadTranslations($translations);
+        }
+        //Cái này phải coi lại
+        // Load theme locale
+        // $path = active_theme("path") . "/locale/" . ACTIVE_LANG . "/messages.po";
+        // if (file_exists($path)) {
+        //     $translations = Gettext\Translations::fromPoFile($path);
+        //     $Translator->loadTranslations($translations);
+        // }
+
+        
+
+        $Translator->register(); // Register global functions
+
+        // Set other library locales
+        try {
+            \Moment\Moment::setLocale(str_replace("-", "_", ACTIVE_LANG));
+        } catch (Exception $e) {
+            // Couldn't load locale
+            // There is nothing to do here,
+            // Fallback to default language
+        }
+    }
     public function process(){
 
         $this -> db();
