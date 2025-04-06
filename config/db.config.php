82d896e7e505405e050860aed99c96abbc7f4591
Nguyen Quoc Anh Quan <nguyenquocanhquangl@gmail.com>
2025-04-07 01:10:12 +0700

    Sua data

--- config/db.config.php
+++ config/db.config.php
@@ -1,11 +1,10 @@
 <?php
 //database config 
 define("DATA_BASE","mysql");
-define("DB_HOST","mysql.railway.internal");
-define("DB_NAME","railway");
+define("DB_HOST","localhost");
+define("DB_NAME","hethongdatlichkham");
 define("DB_CHARSET","utf8");
 define("USER_NAME","root");
-define("DB_PASSWORD","dZKfjvghVHkNaOQLbsDHgoOfRWToKVol");
 //define("","");
 
 //table config 
